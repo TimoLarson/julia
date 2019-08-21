9068707598f10764e1482d33dbf1ea121a0817a0
= <TauLarson@gmail.com>
2019-08-20 23:33:54 -0400

    testing

--- src/dump.c
+++ src/dump.c
@@ -412,9 +412,11 @@ static void jl_serialize_datatype(jl_serializer_state *s, jl_datatype_t *dt) JL_
 
 static void jl_serialize_module(jl_serializer_state *s, jl_module_t *m)
 {
-    if (m->libpath)
-        write_uint8(s->s, TAG_MODULE_COMPILED);
-    else
+    //if (m->libpath) {
+        //jl_printf(JL_STDERR, "m->libpath: %p\n", m);
+        //jl_printf(JL_STDERR, "m->libpath: %p\n", m->libpath);
+    //    write_uint8(s->s, TAG_MODULE_COMPILED);
+    //} else
         write_uint8(s->s, TAG_MODULE);
     jl_serialize_value(s, m->name);
     size_t i;
@@ -473,8 +475,8 @@ static void jl_serialize_module(jl_serializer_state *s, jl_module_t *m)
     write_uint64(s->s, m->build_id);
     write_int32(s->s, m->counter);
     write_int32(s->s, m->nospecialize);
-    if (m->libpath)
-        jl_serialize_value(s, (jl_value_t*)m->libpath);
+    //if (m->libpath)
+    //    jl_serialize_value(s, (jl_value_t*)m->libpath);
 }
 
 static int is_ast_node(jl_value_t *v)
@@ -884,11 +886,8 @@ static void jl_serialize_value_(jl_serializer_state *s, jl_value_t *v, int as_li
         jl_serialize_value(s, (jl_value_t*)mi->cache);
     }
     else if (jl_is_code_instance(v)) {
+        write_uint8(s->s, TAG_CODE_INSTANCE);
         jl_code_instance_t *codeinst = (jl_code_instance_t*)v;
-        if (codeinst->compiled)
-            write_uint8(s->s, TAG_CODE_INST_COMPILED);
-        else
-            write_uint8(s->s, TAG_CODE_INSTANCE);
         int validate = 0;
         if (codeinst->max_world == ~(size_t)0)
             validate = 1; // can check on deserialize if this cache entry is still valid
@@ -907,10 +906,6 @@ static void jl_serialize_value_(jl_serializer_state *s, jl_value_t *v, int as_li
             jl_serialize_value(s, NULL);
             jl_serialize_value(s, NULL);
             jl_serialize_value(s, jl_any_type);
-        }
-        if (codeinst->compiled) {
-            jl_serialize_value(s, codeinst->functionObjectsDecls.functionObject);
-            jl_serialize_value(s, codeinst->functionObjectsDecls.specFunctionObject);
         }
         jl_serialize_value(s, codeinst->next);
     }
@@ -1768,7 +1763,7 @@ static jl_value_t *jl_deserialize_value_method_instance(jl_serializer_state *s, 
     return (jl_value_t*)mi;
 }
 
-static jl_value_t *jl_deserialize_value_code_instance(jl_serializer_state *s, jl_value_t **loc, uint8_t tag) JL_GC_DISABLED
+static jl_value_t *jl_deserialize_value_code_instance(jl_serializer_state *s, jl_value_t **loc) JL_GC_DISABLED
 {
     int usetable = (s->mode != MODE_IR);
     jl_code_instance_t *codeinst =
@@ -1790,10 +1785,6 @@ static jl_value_t *jl_deserialize_value_code_instance(jl_serializer_state *s, jl
     jl_gc_wb(codeinst, codeinst->rettype);
     if (constret)
         codeinst->invoke = jl_fptr_const_return;
-    if (tag == TAG_CODE_INST_COMPILED) {
-        codeinst->functionObjectsDecls.functionObject = (char*)jl_deserialize_value(s, NULL);
-        codeinst->functionObjectsDecls.specFunctionObject = (char*)jl_deserialize_value(s, NULL);
-    }
     codeinst->next = (jl_code_instance_t*)jl_deserialize_value(s, (jl_value_t**)&codeinst->next);
     jl_gc_wb(codeinst, codeinst->next);
     if (validate)
@@ -1801,7 +1792,7 @@ static jl_value_t *jl_deserialize_value_code_instance(jl_serializer_state *s, jl
     return (jl_value_t*)codeinst;
 }
 
-static jl_value_t *jl_deserialize_value_module(jl_serializer_state *s, uint8_t tag) JL_GC_DISABLED
+static jl_value_t *jl_deserialize_value_module(jl_serializer_state *s) JL_GC_DISABLED
 {
     int usetable = (s->mode != MODE_IR);
     uintptr_t pos = backref_list.len;
@@ -1858,12 +1849,6 @@ static jl_value_t *jl_deserialize_value_module(jl_serializer_state *s, uint8_t t
     m->counter = read_int32(s->s);
     m->nospecialize = read_int32(s->s);
     m->primary_world = jl_world_counter;
-
-    if (tag == TAG_MODULE_COMPILED) {
-        m->libpath = (char*)jl_deserialize_value(s, NULL);
-        m->libhandle = jl_dlopen(m->libpath, RTLD_LAZY);
-    }
-
     return (jl_value_t*)m;
 }
 
@@ -2150,14 +2135,10 @@ static jl_value_t *jl_deserialize_value(jl_serializer_state *s, jl_value_t **loc
         return jl_deserialize_value_method(s, loc);
     case TAG_METHOD_INSTANCE:
         return jl_deserialize_value_method_instance(s, loc);
-    case TAG_CODE_INST_COMPILED:
-        return jl_deserialize_value_code_instance(s, loc, tag);
     case TAG_CODE_INSTANCE:
-        return jl_deserialize_value_code_instance(s, loc, tag);
+        return jl_deserialize_value_code_instance(s, loc);
     case TAG_MODULE:
-        return jl_deserialize_value_module(s, tag);
-    case TAG_MODULE_COMPILED:
-        return jl_deserialize_value_module(s, tag);
+        return jl_deserialize_value_module(s);
     case TAG_SHORTER_INT64:
         v = jl_box_int64((int16_t)read_uint16(s->s));
         if (usetable)
